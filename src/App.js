2240fde19c8b70f3e99ba10277677b5d597c863e
jakedheath123 <jakedheath@gmail.com>
2021-01-31 17:27:50 +0000

    webpack config changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>App</h1>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    }),
-    new webpack.ProgressPlugin()
-  ]
+const modeConfig = env => require(`./build-utils/webpack.${env}.js`)(env);
+
+module.exports = ({ env = "production", presets = [] }) => {
+  return merge(
+    {
+      mode: env,
+      entry: "./src",
+      output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js)x?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        }),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(env)
+  );
 };
