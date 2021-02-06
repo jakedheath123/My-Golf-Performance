e41e0896dcb6fdf25de0ad5b7acb57710e65e2df
jakedheath123 <jakedheath@gmail.com>
2021-02-06 16:32:03 +0000

    create initial login page component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    root: {
-      "& > *": {
-        margin: theme.spacing(1)
-      }
-    }
-  })
-);
+import Login from "./components/Login";
 
 const App = () => {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary">
-        App
-      </Button>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 
