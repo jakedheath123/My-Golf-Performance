2fac4b7625f69203184a9abd75d57b0258ab9c35
jakedheath123 <jakedheath@gmail.com>
2021-02-07 13:42:23 +0000

    add rules for css and sass

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,22 @@ module.exports = ({ env = "production", presets = [] }) => {
           {
             test: /\.(js)x?$/,
             exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
+            use: "babel-loader"
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader"
+            ]
           }
         ]
       },
