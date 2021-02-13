4d5db8800c810a0f69fae446fb56e953478c2254
jakedheath123 <jakedheath@gmail.com>
2021-02-13 21:46:39 +0000

    add new components to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@ import React, { FunctionComponent, lazy, Suspense } from "react";
 import { Router } from "@reach/router";
 
 import ErrorBoundary from "./components/ErrorBoundary";
+import NavBar from "./components/NavBar"
+import UserPages from "./components/UserPages"
 
 const Login = lazy(() => import("./components/Login"));
+const GolCourseList = lazy(() => import("./components/GolfCourseList"))
 
 const App: FunctionComponent = () => {
+  
   return (
-    <>
       <ErrorBoundary>
         <Suspense fallback={<p>Loading..</p>}>
           <Router>
             <Login path="/" />
+            <UserPages path="/*">
+              <GolCourseList path="/golfcourselist"/>
+            </UserPages>    
           </Router>
         </Suspense>
       </ErrorBoundary>
-    </>
   );
 };
 
 export default App;
+
