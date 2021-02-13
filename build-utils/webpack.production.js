17a2fbd21d9a231992d6f6328fb34e63e7e399bb
jakedheath123 <jakedheath@gmail.com>
2021-02-13 21:45:25 +0000

    add clean webpack plugin to production webpack config

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,5 +1,8 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = () => ({
   // output: {
   //   filename: "[contenthash].js"
   // }
+  plugins: [new CleanWebpackPlugin()]
 });
