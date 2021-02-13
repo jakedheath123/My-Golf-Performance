9f800bc60aa3dc51a28bbec70cc1ba178e7cc9c4
jakedheath123 <jakedheath@gmail.com>
2021-02-13 21:47:26 +0000

    create boilerplate GolfCourseList component

---
+++ src/components/GolfCourseList.tsx
@@ -0,0 +1,8 @@
+import React, {FunctionComponent} from 'react';
+import { RouteComponentProps } from "@reach/router"
+
+const GolfCourseList: FunctionComponent<RouteComponentProps> = () => (
+  <h1>COURSE LIST</h1>
+)
+
+export default GolfCourseList;
