85edfbeaee5687acaec987dc24731672430f92d6
jakedheath123 <jakedheath@gmail.com>
2021-02-14 20:45:38 +0000

    add starting template styling to list of golf courses

--- src/components/GolfCoursesList.tsx
+++ src/components/GolfCoursesList.tsx
@@ -1,8 +1,36 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import { RouteComponentProps } from "@reach/router"
+import Grid from '@material-ui/core/Grid';
+import Spinner from "./Spinner"
 
-const GolfCoursesList: FunctionComponent<RouteComponentProps> = () => (
-  <h1>COURSE LIST</h1>
-)
+import { GolfCourse } from "../models/golfCourse.modal"
+import { firestore } from "../firebase"
+import GolfCourseCard from "./GolfCourseCard"
+
+const GolfCoursesList: FunctionComponent<RouteComponentProps> = () => {
+  const [golfCourses, setGolfCourses] = useState<GolfCourse[]>([])
+  const [loading, setLoading] = useState(true)
+
+  console.log(golfCourses, "<<<")
+
+  useEffect(() => {
+    firestore.collection("courses").get().then(snapshot => {
+      const courses = snapshot.docs.map(doc => { return { id: doc.id, ...doc.data() }; })
+        setGolfCourses(courses)
+        setLoading(false)
+       }
+      )
+}, [])
+
+  if (loading) return <Spinner />
+
+  return (
+    <Grid container alignItems="center" justify="center" direction="column">
+    {golfCourses.map(course => (
+      <GolfCourseCard key={course.id} course={course}/>
+    ))}
+    </Grid>
+  )
+}
 
 export default GolfCoursesList;
