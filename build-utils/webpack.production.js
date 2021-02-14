12fdf3e4e17039cb969555646fa1a5a589b3865e
jakedheath123 <jakedheath@gmail.com>
2021-02-14 12:06:21 +0000

    uninstall clean-webpack-plugin

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,8 +1,5 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
 module.exports = () => ({
   // output: {
   //   filename: "[contenthash].js"
   // }
-  plugins: [new CleanWebpackPlugin()]
 });
