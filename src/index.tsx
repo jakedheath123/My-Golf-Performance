60825116ad0e76434415fc54f27967cbe52c1fe1
jakedheath123 <jakedheath@gmail.com>
2021-02-15 21:22:42 +0000

    acreate redux store to provide to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/style.scss";
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import ReduxThunk from "redux-thunk"
 
+import reducers from "./redux/reducers"
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(ReduxThunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById("root"));
