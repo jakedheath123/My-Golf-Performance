cc69d05586ac226003e32de461deb28c540f7518
jakedheath123 <jakedheath@gmail.com>
2021-02-15 21:24:21 +0000

    bring in useFirestore custom hook

--- src/components/GolfCoursesList.tsx
+++ src/components/GolfCoursesList.tsx
@@ -1,26 +1,13 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
+import React, { FunctionComponent } from 'react';
 import { RouteComponentProps } from "@reach/router"
 import Grid from '@material-ui/core/Grid';
 import Spinner from "./Spinner"
 
-import { GolfCourse } from "../models/golfCourse.modal"
-import { firestore } from "../firebase"
+import { useFirestore } from "../hooks/useFirestore"
 import GolfCourseCard from "./GolfCourseCard"
 
 const GolfCoursesList: FunctionComponent<RouteComponentProps> = () => {
-  const [golfCourses, setGolfCourses] = useState<GolfCourse[]>([])
-  const [loading, setLoading] = useState(true)
-
-  console.log(golfCourses, "<<<")
-
-  useEffect(() => {
-    firestore.collection("courses").get().then(snapshot => {
-      const courses = snapshot.docs.map(doc => { return { id: doc.id, ...doc.data() }; })
-        setGolfCourses(courses)
-        setLoading(false)
-       }
-      )
-}, [])
+  const [golfCourses, loading, error ] = useFirestore("courses")
 
   if (loading) return <Spinner />
 
