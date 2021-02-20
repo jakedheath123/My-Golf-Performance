87e77539a788c30d909716dc687a5d71ed5d40c6
jakedheath123 <jakedheath@gmail.com>
2021-02-20 20:46:08 +0000

    remove sign in elements from component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,75 +1,31 @@
 import React, { FunctionComponent } from "react"
 import { RouteComponentProps } from "@reach/router"
-import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import PersonIcon from '@material-ui/icons/Person';
-import LockIcon from '@material-ui/icons/Lock';
+import Container from "@material-ui/core/Container";
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import Typography from '@material-ui/core/Typography';
-import FacebookIcon from '@material-ui/icons/Facebook';
 
 import Authentication from "./Authentication"
+import golferMale from "../images/golfer-male"
+import golferFemale from "../images/golfer-female"
 
 const Login: FunctionComponent<RouteComponentProps> = () => { 
   return (
-    <div className="login" data-test="component-login">
+    <div className="login">
+    <Container className="login-container" disableGutters={true} maxWidth={false}>
       <Paper className="login__form">
       <form >
       <Grid container justify="center">
-          <Grid item className="login__icon-buttons--spacing">
       <Authentication />
       </Grid>  
-      </Grid>
-      <Grid container justify="center">
-          <Grid item className="login__icon-buttons--spacing">
-        <Button 
-        className="login__icon-buttons"
-        disableElevation
-        startIcon={<FacebookIcon />}
-        variant="contained"
-        color="primary">
-      Sign in with Facebook
-      </Button>
-      </Grid>  
-      </Grid>
-      <Grid container justify="center">
-            <Grid item>
-            <Typography variant="subtitle1">Or</Typography>
-          </Grid>
-      </Grid>      
-      <Grid container spacing={1} alignItems="flex-end" justify="center" >
-          <Grid item >
-            <PersonIcon />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Username" />
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} alignItems="flex-end" justify="center">
-          <Grid item>
-            <LockIcon />
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Password" />
-          </Grid>
-          <Grid container justify="center">
-            <Grid item className="login__icon-buttons--spacing">
-            <Button
-            disableElevation
-            size="medium"
-        variant="contained"
-        color="default"
-        endIcon={<ExitToAppIcon />}
-      >
-        Login
-      </Button>
-            </Grid>
-          </Grid>
-        </Grid>
         </form>
+        <div className="tester">
+      <img src={golferMale} alt="Golfer male" className="login-container_male-img"/>
+      </div>
         </Paper>
+      <div className="tester2">
+      <img src={golferFemale} alt="Golfer female" className="login-container_female-img"/>
+      </div>
+    </Container>
     </div>
   );
 };
