b43be44b78c0cfab7477ff148ed057a426f4ea8c
jakedheath123 <jakedheath@gmail.com>
2021-02-28 19:07:52 +0000

    import golfCoursesReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 
 import userAuthenticationReducer from "./userAuthenticationReducer";
+import userInterfaceReducer from "./userInterfaceReducer";
+import golfCoursesReducer from "./golfCoursesReducer";
 
 export default combineReducers({
-  userAuthentication: userAuthenticationReducer
+  userAuthentication: userAuthenticationReducer,
+  userInterface: userInterfaceReducer,
+  golfCourses: golfCoursesReducer
 });
