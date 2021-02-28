22df0b340a2b098eb9c89ebd2c443f4ebe097e7d
jakedheath123 <jakedheath@gmail.com>
2021-02-28 18:58:57 +0000

    create ConfirmModal component

---
+++ src/components/ConfirmModal.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { useSelector } from "react-redux";
+
+const ConfirmModal = ({ open, handleClose, children}) => {
+  const classes = useStyles();
+  const { toggleModal } = useSelector(state => state.userInterface)
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          {children}
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }),
+);
+
+export default ConfirmModal;
