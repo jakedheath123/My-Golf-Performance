cff2ec5830163eed580467030ad7e0952ce1e083
jakedheath123 <jakedheath@gmail.com>
2021-03-11 17:33:59 +0000

    add Sidebar

--- src/components/UserPagesContainer.tsx
+++ src/components/UserPagesContainer.tsx
@@ -1,8 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { RouteComponentProps } from "@reach/router" 
+import { useSelector } from "react-redux";
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 import NavBar from "./NavBar"
+import SideBar from "./SideBar"
 
 interface UserPagesContainerProps extends RouteComponentProps  {
   url?: string
@@ -10,12 +15,15 @@ interface UserPagesContainerProps extends RouteComponentProps  {
 
 const UserPagesContainer: FunctionComponent<UserPagesContainerProps> = (props) => (
   <>
-   <NavBar/> 
-   <Grid container alignItems="center" justify="center" direction="column" className="user-pages">
-     <Grid item>
-      {props.children}
+   <NavBar/>
+   <div style={{position: "relative"}}>
+     <Grid container direction="column" className="user-pages">
+      <Grid item>
+        {props.children}
+      </Grid>
      </Grid>
-   </Grid>
+     <SideBar />
+   </div> 
   </>
 )
 
