2bd067f023c27b3663fc33d92a2f23920964e079
jakedheath123 <jakedheath@gmail.com>
2021-03-14 15:49:12 +0000

    remove SIGN_UP_LINK_CLICKED case and key from state

--- src/redux/reducers/userInterfaceReducer.js
+++ src/redux/reducers/userInterfaceReducer.js
@@ -1,17 +1,21 @@
 import {
   SIGN_IN_CLICKED,
   SET_MENU_CLICKED,
-  COURSE_SELECTED
+  COURSE_SELECTED,
+  SIGN_UP_LINK_CLICKED
 } from "../actions/constants";
 
 const INITIAL_STATE = {
   signInClicked: false,
   menuClicked: false,
-  courseSelected: null
+  courseSelected: null,
+  signUpLinkClicked: true
 };
 
 const userInterfaceReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case SIGN_UP_LINK_CLICKED:
+      return { ...state, signUpLinkClicked: !state.signUpLinkClicked };
     case COURSE_SELECTED:
       return { ...state, courseSelected: action.payload };
     case SET_MENU_CLICKED:
